51e3f4e7f009a322bad3cb1b57c6166731f740a1
romanjsonar <roman@jsonar.com>
2017-05-30 11:27:41 -0700

    remove visualizations, dashboard, timeloin plugin, switch icon
    Conflicts:
    	src/core_plugins/timelion/index.js

--- src/core_plugins/timelion/index.js
+++ src/core_plugins/timelion/index.js
@@ -1,6 +1,6 @@
 
 module.exports = function (kibana) {
-  let mainFile = 'plugins/timelion/app';
+  let mainFile = 'plugins/timelion/app';// eslint-disable-line no-unused-vars
 
   const ownDescriptor = Object.getOwnPropertyDescriptor(kibana, 'autoload');
   const protoDescriptor = Object.getOwnPropertyDescriptor(kibana.constructor.prototype, 'autoload');
@@ -11,32 +11,6 @@ module.exports = function (kibana) {
     mainFile = 'plugins/timelion/app_with_autoload';
   }
 
-  return new kibana.Plugin({
-    require: ['kibana', 'elasticsearch'],
-    uiExports: {
-      app: {
-        title: 'Timelion',
-        order: -1000,
-        description: 'Time series expressions for everything',
-        icon: 'plugins/timelion/icon.svg',
-        main: mainFile,
-        injectVars: function (server) {
-          const config = server.config();
-          return {
-            kbnIndex: config.get('kibana.index'),
-            esShardTimeout: config.get('elasticsearch.shardTimeout'),
-            esApiVersion: config.get('elasticsearch.apiVersion')
-          };
-        }
-      },
-      hacks: [
-        'plugins/timelion/lib/panel_registry',
-        'plugins/timelion/panels/timechart/timechart'
-      ],
-      visTypes: [
-        'plugins/timelion/vis'
-      ]
-    },
-    init: require('./init.js'),
-  });
+  return;
+
 };

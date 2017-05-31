936b3fc6cb887c35ad47d20ece2b589982ce1139
romanjsonar <roman@jsonar.com>
2017-05-30 17:55:31 -0700

    cleanup and logging

--- tasks/build/for_sonar.js
+++ tasks/build/for_sonar.js
@@ -2,13 +2,19 @@ export default (grunt) => {
 
   grunt.registerTask('_build:forSonar', 'handle Sonar specific options', function () {
 
+    const dest = grunt.config.get('copy.devSource.dest');
+
+    grunt.log.writeln('Using destination: ' + dest);
+
     if (grunt.option('with-visualizations')) {
-      grunt.file.copy('src/core_plugins/kibana/index.js.orig', 'src/core_plugins/kibana/index.js');
-      grunt.file.copy('src/core_plugins/timelion/index.js.orig', 'src/core_plugins/timelion/index.js');
+      grunt.log.writeln('Enable visualizations');
+      grunt.file.copy(dest + '/src/core_plugins/kibana/index.js.orig', dest + '/src/core_plugins/kibana/index.js');
+      grunt.file.copy(dest + '/src/core_plugins/timelion/index.js.orig', dest + '/src/core_plugins/timelion/index.js');
     }
     else {
-      grunt.file.copy('src/core_plugins/kibana/index.js.new', 'src/core_plugins/kibana/index.js');
-      grunt.file.copy('src/core_plugins/timelion/index.js.new', 'src/core_plugins/timelion/index.js');
+      grunt.log.writeln('Disable visualizations');
+      grunt.file.copy(dest + '/src/core_plugins/kibana/index.js.new', dest + '/src/core_plugins/kibana/index.js');
+      grunt.file.copy(dest + '/src/core_plugins/timelion/index.js.new', dest + '/src/core_plugins/timelion/index.js');
     }
   });
 };
